689707669ae2a9954a39ba8cd6574fefe9e92852
new <nneew0055@gmail.com>
2022-11-08 19:44:02 +0700

    up_login

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <img src="./images/ref.jpg">
-</body>
-</html>
+<?php 
+
+session_start();
+
+	if(!isset($_SESSION['userlogin'])){
+		header("Location: login.php");
+	}
+
+	if(isset($_GET['logout'])){
+		session_destroy();
+		unset($_SESSION);
+		header("Location: login.php");
+	}
+
+?>
+
+<p>Welcome to index</p>
+
+
+<a href="index.php?logout=true">Logout</a>
