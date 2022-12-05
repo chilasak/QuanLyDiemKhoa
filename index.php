56e890b58f3f92078884569a2030d7442f774ae3
new <nneew0055@gmail.com>
2022-12-05 08:36:28 +0700

    my task

--- index.php
+++ index.php
@@ -1,20 +1,27 @@
 <?php 
+require_once './Connect/connect.php';
+Database_ql_diem::Connect();
+if (isset($_GET['controllers'])) {
+	$controllers = $_GET['controllers'];
+}
+else
+{
+	$controllers = NULL;
+}
+switch ($controllers) {
+	case 'login':
+		require_once 'Controllers/Login/dangnhap.php';
+		break;
+	case 'quanly':
+		require_once 'Controllers/Quanly/Bang.php';
+		break;
+	case 'diem':
+		require_once 'Controllers/Quanly/Diem.php';
+		break;
+	default:
+		// echo "controllers không tồn tại";
+		require_once 'Controllers/Login/dangnhap.php';
+		break;
+}
 
-session_start();
-
-	if(!isset($_SESSION['userlogin'])){
-		header("Location: login.php");
-	}
-
-	if(isset($_GET['logout'])){
-		session_destroy();
-		unset($_SESSION);
-		header("Location: login.php");
-	}
-
-?>
-
-<p>Welcome to index</p>
-
-
-<a href="index.php?logout=true">Logout</a>
+ ?>
